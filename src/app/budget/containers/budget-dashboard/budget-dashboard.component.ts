752ab8ee616ba13988e994676e8cd03fd0eea16b
Sebastian Denis <sebastian.denis@outlook.com>
2018-06-20 11:41:27 +0200

    0.35.0

--- src/app/budget/containers/budget-dashboard/budget-dashboard.component.ts
+++ src/app/budget/containers/budget-dashboard/budget-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { User } from '../../../user/models/user.model';
 import { Budget } from '../../models/budget.model';
